a903123ef761d27faa5944384e0b42c03e75c888
Daniel Park <37126844+DanPark13@users.noreply.github.com>
2020-12-04 11:37:20 -0500

    End of 12/4/20 Class: Attempted the Simulation of a Bounce

--- bouncing_y.m
+++ bouncing_y.m
@@ -3,24 +3,43 @@ function [time, height] = bouncing_y(D_0)
 %   Detailed explanation goes here
 
 %parameters
-g = -9.81;
-Radius = 0.001;
+g = -9.81; % m/s^2
+Radius = 0.001; % meters
+
 %simulation parameters
 t_0 = 0;
-t_fin = 60*10;  %10 minutes
+t_fin = 60;  %1 minutes
 
 init = [D_0, 0];    %initial distance, initial velocity
+
+
 endtime = odeset('Events', @event_func, 'RelTol', 1e-5);
 
+[time1, height1] = ode45(@fall, [t_0, t_fin], init, endtime); 
 
-[time, height] = ode45(@fall, [t_0, t_fin], init, endtime); 
-    
+% height(length(height),2) = -height(length(height),2);
+
+[time2, height2] = ode45(@rise, [t_fin, 120], [0.002, -height1(length(height1))], endtime); 
+
+time = [time1;time2];
+height = [height1; height2];
+
     function res = fall(~, U)
         y = U(1);
         v = U(2);
         
         dvdt = g;
         dydt = v;
+        
+        res = [dydt; dvdt];
+    end
+
+    function res = rise(~, U)
+        y = U(1);
+        v = U(2);
+        
+        dydt = v;
+        dvdt = g;
         
         res = [dydt; dvdt];
     end
@@ -34,4 +53,3 @@ endtime = odeset('Events', @event_func, 'RelTol', 1e-5);
         direction = -1;
     end
 end
-
