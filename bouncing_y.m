fea4d090c45d9606170e821cab73224c24c07d81
ccolizzi <ccolizzi@JHPRM53.olin.edu>
2020-12-04 16:26:41 +0100

    added ODEs in the Y direction. Created event function that identifies when the ball hits the ground.

--- bouncing_y.m
+++ bouncing_y.m
@@ -1,19 +1,37 @@
-function [time, height] = bouncing_y(initialheight, mass)
+function [time, height] = bouncing_y(D_0)
 %BOUNCING Summary of this function goes here
 %   Detailed explanation goes here
 
 %parameters
-g = 9.81;
-
+g = -9.81;
+Radius = 0.001;
 %simulation parameters
 t_0 = 0;
 t_fin = 60*10;  %10 minutes
-v_0 = 0;
 
-[time, height] = ode45(@bounce, [t_0, t_fin], v_0); %need to add event func
+init = [D_0, 0];    %initial distance, initial velocity
+endtime = odeset('Events', @event_func, 'RelTol', 1e-5);
 
-    function res = bounce(~, U)
-        v = v_0 + (g*time)
+
+[time, height] = ode45(@fall, [t_0, t_fin], init, endtime); 
+    
+    function res = fall(~, U)
+        y = U(1);
+        v = U(2);
+        
+        dvdt = g;
+        dydt = v;
+        
+        res = [dydt; dvdt];
+    end
+
+    function [value, isterminal, direction] = event_func(~, U)
+        y = U(1);
+        y_fin = Radius;
+        
+        value = y - y_fin;
+        isterminal = 1;
+        direction = -1;
     end
 end
 
