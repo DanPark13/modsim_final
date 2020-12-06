77ea6d3ff7706369bb550a4db2f322c3071f2e34
ccolizzi <ccolizzi@JHPRM53.olin.edu>
2020-12-07 00:38:17 +0100

    Implemented ODE45 code in both x and y dimension

--- bouncing_y.m
+++ bouncing_y.m
@@ -13,17 +13,14 @@ t_fin = 60;  %1 minutes
 
 init = [D_0, 0];    %initial distance, initial velocity
 
-x_prev_pos = 1.428; %previous position of x value where it lands
-
 options = odeset('Events', @event_func, 'RelTol', 2, "Refine", 10);
 
 [time1, height1] = ode45(@fall, [t_0, t_fin], init, options); 
 
 % height(length(height),2) = -height(length(height),2);
 
-[time2, height2] = ode45(@rise, [x_prev_pos, t_fin], [0, cor*-height1(length(height1), 2)], options);
+[time2, height2] = ode45(@rise, [time1(length(time1)), t_fin], [0, cor*-height1(length(height1), 2)], options);
 
-%%%%%%%%%%%%%%% HOW DO WE MAKE THE 1.428 automated?
 
 time = [time1;time2];
 height = [height1; height2];
