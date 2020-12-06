c0fd32796f1a0e80e332d1a9ec61b67de400d7c7
Daniel Park <37126844+DanPark13@users.noreply.github.com>
2020-12-06 17:57:35 -0500

    12/6/20 | 5:57 PM | Removed some extraneous information

--- ball_bounce.m
+++ ball_bounce.m
@@ -30,26 +30,7 @@ t = 0;      % s (Time)
 x(1) = 0;   % m (Initial position of ball in x direction)
 y(1) = 0;   % m (Initial position of ball in y direction)
 
-%% Calculated Variables
-
-x_pos_prev = x(end);            % Get position previous of ball
-y_v = y_v * cor;                % Multiply coefficient of resitution to position
-flight_time = 2 * y_v / g;      % First equation (v = u + at --> (v - u)/a = t)
-time_increment = flight_time / 20;   % Time Increment (20 Steps)
-t = 0;
-
 %% Simulation
-
-while t < flight_time
-    t = t + time_increment;
-    
-    if((y_v - 0.5 * g * t) * t >= 0)
-        x(end + 1) = x_v * t;
-        y(end + 1) = (y_v - 0.5 * g * t) * t;
-    end
-end
-
-%% Calculated Variables
 
 while y_v > 0.0000001
     
@@ -58,8 +39,6 @@ while y_v > 0.0000001
     flight_time = 2 * y_v / g;      % First equation (v = u + at --> (v - u)/a = t)
     time_increment = flight_time / 20;   % Time Increment (20 Steps)
     t = 0;
-
-%% Simulation
 
     while t < flight_time
         t = t + time_increment;
