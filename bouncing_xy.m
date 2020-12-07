2441e12bdbbdc061a55a94505eda967c4f527f6e
ccolizzi <ccolizzi@JHPRM53.olin.edu>
2020-12-07 01:16:55 +0100

    added multiple bouces to the xy and automated the variable naming

--- bouncing_xy.m
+++ bouncing_xy.m
@@ -10,20 +10,22 @@ distance(1) = 0;
 
 %simulation parameters
 t_0 = 0;
-t_fin = 60;  %1 minutes
+t_fin = 60*10;  %1 minutes
 
 init = [D_0, 0];    %initial distance, initial velocity
 
 options = odeset('Events', @event_func, 'RelTol', 2, "Refine", 10);
 
-[time1, height1] = ode45(@fall, [t_0, t_fin], init, options); 
+[time, height] = ode45(@fall, [t_0, t_fin], init, options); 
 
 % height(length(height),2) = -height(length(height),2);
-
-[time2, height2] = ode45(@rise, [time1(length(time1)), t_fin], [0, cor*-height1(length(height1), 2)], options);
-
-time = [time1;time2];
-height = [height1; height2];
+for jump = 2:20
+    [time2, height2] = ode45(@rise, [time(end), t_fin], [0, cor*-height(end, 2)], options);
+    time = [time; time2];
+    height = [height; height2];
+end
+%time = [time1;time2];
+%height = [height1; height2];
 
 for i = 2: length(time)
     distance(end + 1) = VX_0 * time(i);
